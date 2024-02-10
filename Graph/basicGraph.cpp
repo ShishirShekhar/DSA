bd312f8c338d6a6b1b5a606c1eac9c89ade3c7e8
Shishir Shekhar <sspdav02@gmail.com>
2024-02-10 13:01:57 +0530

    basic graph

---
+++ Graph/basicGraph.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <list>
+using namespace std;
+
+class Graph
+{
+    int v;
+    list<int> *l;
+
+public:
+    Graph(int v)
+    {
+        this->v = v;
+        l = new list<int>[v];
+    }
+
+    void addEdge(int i, int j, bool undir = true)
+    {
+        l[i].push_back(j);
+        if (undir)
+        {
+            l[j].push_back(i);
+        }
+    }
+
+    void printAdjList()
+    {
+        for (int i = 0; i < v; i++)
+        {
+            cout << i << "-->";
+            for (auto node : l[i])
+            {
+                cout << node << ", ";
+            }
+            cout << "\n";
+        }
+    }
+};
+
+int main()
+{
+    Graph g(6);
+    g.addEdge(0, 1);
+    g.addEdge(0, 4);
+    g.addEdge(1, 2);
+    g.addEdge(4, 3);
+    g.addEdge(4, 5);
+    g.addEdge(2, 5);
+    g.addEdge(3, 5);
+
+    g.printAdjList();
+}
