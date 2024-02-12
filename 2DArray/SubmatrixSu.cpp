814875a40346962b1b589c4a90c45aab478bcfde
Shishir Shekhar <sspdav02@gmail.com>
2024-02-12 15:34:06 +0530

    sub matrix sum

---
+++ 2DArray/SubmatrixSu.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int sum(vector<vector<int>> v, int sr, int sc, int er, int ec)
+{
+    // your code goes here
+    int sum = 0;
+
+    for (int i = sr; i <= er; i++)
+    {
+        for (int j = sc; j <= ec; j++)
+        {
+            sum += v[i][j];
+        }
+    }
+
+    return sum;
+}
